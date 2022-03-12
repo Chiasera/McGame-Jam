16fbccea2225da340b8656302d139d22fc31728d
Michell203 <michaelhijazeen203@gmail.com>
2022-03-12 12:57:11 -0500

    new script

---
+++ Mc_Game_Jam_2022/Assets/Scripts/pixelCharacterMovement.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(BoxCollider2D))]
+public class pixelCharacterMovement : MonoBehaviour
+{
+
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+    
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        
+        // Reset moveDelta
+        moveDelta = new Vector3(x,y,0);
+        
+        // Swap sprite direction, whether going right or left
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        } else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        
+        //Let's make it move
+        transform.Translate(moveDelta * Time.deltaTime);
+
+    }
+    
+}
